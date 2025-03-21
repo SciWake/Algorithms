1fc31fdd826005ac2b208905f62fbef1d9f92cd9
SciWake <SciWake@gmail.com>
2025-03-22 00:56:21 +0300

    Добавлен namespace std

--- yandex_training_1_cpp/1_complexity_listening_unexpected_cases/homework/A_air_conditioning.cpp
+++ yandex_training_1_cpp/1_complexity_listening_unexpected_cases/homework/A_air_conditioning.cpp
@@ -1,26 +1,29 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
+
+
 int main() {
     int troom, tcond;
-    std::string mode;
+    string mode;
 
-    std::cin >> troom >> tcond;
-    std::cin >> mode;
+    cin >> troom >> tcond;
+    cin >> mode;
 
     int result;
 
     if (mode == "freeze") {  // Только охлаждение
-        result = std::min(troom, tcond);
+        result = min(troom, tcond);
     } else if (mode == "heat") {  // Только нагрев
-        result = std::max(troom, tcond);
+        result = max(troom, tcond);
     } else if (mode == "auto") {  // Автоматически доводит до нужной температуры
         result = tcond;
     } else if (mode == "fan") {  // Только вентиляция, температура не меняется
         result = troom;
     }
 
-    std::cout << result << std::endl;
+    cout << result << endl;
 
     return 0;
 }
