55e9ae3871ab62a63357896886a4bbd005b29651
SciWake <SciWake@gmail.com>
2025-03-23 02:30:30 +0300

    Более компактое решение

--- yandex_training_1_cpp/1_complexity_listening_unexpected_cases/A. Кондиционер/main.cpp
+++ yandex_training_1_cpp/1_complexity_listening_unexpected_cases/A. Кондиционер/main.cpp
@@ -5,10 +5,8 @@ using namespace std;
 
 
 int ConditionerOne(int t_room, int t_cond, const string& mode) {
-    if (mode == "auto" || (t_room < t_cond && mode == "heat") || (t_room > t_cond && mode == "freeze")) {
-        return t_cond;
-    }
-    return t_room;
+    return (mode == "auto") || (mode == "heat" && t_room < t_cond) || (mode == "freeze" && t_room > t_cond)
+           ? t_cond : t_room;
 }
 
 
