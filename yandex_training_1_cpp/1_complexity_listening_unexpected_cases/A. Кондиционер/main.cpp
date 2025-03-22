0345a4973d85da5f2f6f9c91a557c4da824296b3
SciWake <SciWake@gmail.com>
2025-03-23 02:22:53 +0300

    Добавлено более простое решение

--- yandex_training_1_cpp/1_complexity_listening_unexpected_cases/A. Кондиционер/main.cpp
+++ yandex_training_1_cpp/1_complexity_listening_unexpected_cases/A. Кондиционер/main.cpp
@@ -4,26 +4,27 @@
 using namespace std;
 
 
+int ConditionerOne(int t_room, int t_cond, const string& mode) {
+    if (mode == "auto" || (t_room < t_cond && mode == "heat") || (t_room > t_cond && mode == "freeze")) {
+        return t_cond;
+    }
+    return t_room;
+}
+
+
+int ConditionerTwo(int t_room, int t_cond, const string& mode) {
+    return mode == "auto" ? t_cond :
+           mode == "heat" ? max(t_room, t_cond) :
+           mode == "freeze" ? min(t_room, t_cond) :
+           t_room;
+}
+
+
 int main() {
     int troom, tcond;
     string mode;
 
     cin >> troom >> tcond;
     cin >> mode;
-
-    int result;
-
-    if (mode == "freeze") {  // Только охлаждение
-        result = min(troom, tcond);
-    } else if (mode == "heat") {  // Только нагрев
-        result = max(troom, tcond);
-    } else if (mode == "auto") {  // Автоматически доводит до нужной температуры
-        result = tcond;
-    } else if (mode == "fan") {  // Только вентиляция, температура не меняется
-        result = troom;
-    }
-
-    cout << result << endl;
-
-    return 0;
+    cout << ConditionerTwo(troom, tcond, mode) << endl;
 }
