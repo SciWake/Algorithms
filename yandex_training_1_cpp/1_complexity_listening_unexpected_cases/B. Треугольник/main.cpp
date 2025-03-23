66bca5bccdf495a9e37f8f85c1a47db162b7237d
SciWake <SciWake@gmail.com>
2025-03-23 03:22:30 +0300

    Запишем через тернарный оператор

--- yandex_training_1_cpp/1_complexity_listening_unexpected_cases/B. Треугольник/main.cpp
+++ yandex_training_1_cpp/1_complexity_listening_unexpected_cases/B. Треугольник/main.cpp
@@ -3,13 +3,9 @@
 
 using namespace std;
 
+
 int main() {
     int a, b, c;
     cin >> a >> b >> c;
-
-    if (a + b > c && a + c > b && c + b > a) {
-        cout << "YES" << endl;
-    } else {
-        cout << "NO" << endl;
-    }
+    cout << ((a + b > c && a + c > b && c + b > a) ? "YES" : "NO");
 }
