6d7c1508b3d8e3518a01aad3bb39fa41187ecf6c
SciWake <SciWake@gmail.com>
2025-06-18 17:51:25 +0300

    Rename files

--- yandex_training_1_cpp/linear_search/ltask_1.cpp
+++ yandex_training_1_cpp/linear_search/ltask_1.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 
 // Первое левое вхождение
-int left_search(vector<int> numbers, int search_number) {
+int FindFirstOccurrence(vector<int> numbers, int search_number) {
     int search_index = -1;
     for (size_t i = 0; i < numbers.size(); i++) {
         if (numbers[i] == search_number && search_index == -1) {
@@ -18,8 +18,8 @@ int left_search(vector<int> numbers, int search_number) {
     cout << search_index << endl;
 }
 
-// Первое правео вхождение
-int right_search(vector<int> numbers, int search_number) {
+// Первое правео вхождение, проход с начала
+int FindLastOccurrence(vector<int> numbers, int search_number) {
     int search_index = -1;
     for (size_t i = 0; i < numbers.size(); i++) {
         if (numbers[i] == search_number) {
@@ -32,6 +32,6 @@ int right_search(vector<int> numbers, int search_number) {
 
 int main() {
     vector<int> numbers = {2, 5, 1, 8, 9, 2, 1, 1};
-    cout << "Left searhc result: "s << left_search(numbers, 1) << endl;
-    cout << "Right searhc result: "s << left_search(numbers, 1) << endl;
+    cout << "Left searhc result: "s << FindFirstOccurrence(numbers, 1) << endl;
+    cout << "Right searhc result: "s << FindLastOccurrence(numbers, 1) << endl;
 }
