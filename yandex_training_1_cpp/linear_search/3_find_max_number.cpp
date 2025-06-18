49a21352f44c914296e62592b1b068017b2f2f5b
SciWake <SciWake@gmail.com>
2025-06-18 19:02:01 +0300

    Solve the problem

---
+++ yandex_training_1_cpp/linear_search/3_find_max_number.cpp
@@ -0,0 +1,36 @@
+// Дана последовательность чисел диной N (N > 0). Найти максимальное число в последовательности.
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+// Может приводить к копирвоанию, если объект не примитив int, double...
+int FindMaxNumber(const vector<int>& numbers) {
+    int max_number = numbers[0];
+    for (size_t i = 1; i < numbers.size(); i++) {
+        if (numbers[i] > max_number) {
+            max_number = numbers[i];
+        }
+    }
+    return max_number;
+}
+
+// Без копирования объектов
+size_t FindMaxNumberDontCopy(const vector<int>& numbers) {
+    size_t max_index = 0;
+    for (size_t i = 1; i < numbers.size(); i++) {
+        if (numbers[i] > numbers[max_index]) {
+            max_index = i;
+        }
+    }
+    return max_index;
+}
+
+
+int main() {
+    vector<int> numbers = {2, 5, 1, 8, 9, 2, 1, 1};
+    cout << "FindMaxNumber: "s << FindMaxNumber(numbers) << endl;
+    cout << "FindMaxNumberDontCopy: "s << FindMaxNumberDontCopy(numbers) << endl;
+}
