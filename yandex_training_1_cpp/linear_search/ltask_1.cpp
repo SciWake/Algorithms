6f99f09e850eb1597a207643952a3aaeabe04e4e
SciWake <SciWake@gmail.com>
2025-06-18 18:01:17 +0300

    Add FindLastOccurrenceReverse function

--- yandex_training_1_cpp/linear_search/ltask_1.cpp
+++ yandex_training_1_cpp/linear_search/ltask_1.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 
-// Первое левое вхождение
+// Первое левое вхождение, проход с начала
 int FindFirstOccurrence(vector<int> numbers, int search_number) {
     int search_index = -1;
     for (size_t i = 0; i < numbers.size(); i++) {
@@ -15,7 +15,7 @@ int FindFirstOccurrence(vector<int> numbers, int search_number) {
             search_index = static_cast<int>(i);
         }
     }
-    cout << search_index << endl;
+    return search_index;
 }
 
 // Первое правео вхождение, проход с начала
@@ -26,12 +26,24 @@ int FindLastOccurrence(vector<int> numbers, int search_number) {
             search_index = static_cast<int>(i);
         }
     }
-    cout << search_index << endl;
+    return search_index;
+}
+
+// Первое правое вхождение, проход с конца
+int FindLastOccurrenceReverse(vector<int> numbers, int search_number) {
+    int search_index = -1;
+    for (size_t i = numbers.size() - 1; i >= 0; i--) {
+        if (numbers[i] == search_number && search_index == -1) {
+            search_index = static_cast<int>(i);
+        }
+    }
+    return search_index;
 }
 
 
 int main() {
     vector<int> numbers = {2, 5, 1, 8, 9, 2, 1, 1};
-    cout << "Left searhc result: "s << FindFirstOccurrence(numbers, 1) << endl;
-    cout << "Right searhc result: "s << FindLastOccurrence(numbers, 1) << endl;
+    cout << "LFindFirstOccurrence: "s << FindFirstOccurrence(numbers, 1) << endl;
+    cout << "FindLastOccurrence: "s << FindLastOccurrence(numbers, 1) << endl;
+    cout << "FindLastOccurrenceReverse: "s << FindLastOccurrence(numbers, 21) << endl;
 }
