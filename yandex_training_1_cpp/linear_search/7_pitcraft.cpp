35b4571089630678592df3bb3903b406b9be32db
SciWake <SciWake@gmail.com>
2025-06-19 18:07:45 +0300

    Update the name of the variables

--- yandex_training_1_cpp/linear_search/7_pitcraft.cpp
+++ yandex_training_1_cpp/linear_search/7_pitcraft.cpp
@@ -8,38 +8,43 @@
 
 using namespace std;
 
-int CalculateTrappedWater(const vector<int>& relief) {
-    size_t global_max_pos = 0;
-    for (size_t i = 0; i < relief.size(); i++) {
-        if (relief[i] > relief[global_max_pos]) {
-            global_max_pos = i;
+
+int CalculateTrappedWater(const vector<int>& heights) {
+    if (heights.empty()) return 0;
+
+    size_t peak_index = 0;
+    for (size_t i = 0; i < heights.size(); i++) {
+        if (heights[i] > heights[peak_index]) {
+            peak_index = i;
         }
     }
 
-    int count_water = 0;
-    // Проход слева до максимума
-    int local_max = 0;
-    for (size_t i = 0; i < global_max_pos; i++) {
-        if (relief[i] > local_max) {
-            local_max = relief[i];
+    int trapped_water = 0;
+
+    // Слева до пика
+    int left_max = 0;
+    for (size_t i = 0; i < peak_index; i++) {
+        if (heights[i] > left_max) {
+            left_max = heights[i];
         }
-        count_water += local_max - relief[i];
+        trapped_water += left_max - heights[i];
     }
-    
-    // Проход справа до максимума
-    local_max = 0;
-    for (size_t i = relief.size() - 1; i > global_max_pos; i--) {
-        if (relief[i] > local_max) {
-            local_max = relief[i];
+
+    // Справа до пика
+    int right_max = 0;
+    for (size_t i = heights.size() - 1; i > peak_index; i--) {
+        if (heights[i] > right_max) {
+            right_max = heights[i];
         }
-        count_water += local_max - relief[i];
+        trapped_water += right_max - heights[i];
     }
-    return count_water;
+
+    return trapped_water;
 }
 
 
 int main() {
-    vector<int> relief_1 = {2, 5, 2, 3, 6, 9, 3, 1, 3, 4, 6}; // 18
+    vector<int> relief_1 = {}; // 18
     cout << CalculateTrappedWater(relief_1) << endl;
     vector<int> relief_2 = {1, 2, 3, 4, -11019, 4, 3, 2, 1}; // # 11023
     cout << CalculateTrappedWater(relief_2) << endl;
